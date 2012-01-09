eed38690424320bde9a4ed68304d89d246e9f130
Henrik Hodne <dvyjones@dvyjones.com>
2012-01-09 01:10:21 +0100

    "Correct" is redundant in specs.

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -8,7 +8,7 @@ describe ContextIO::Account do
       accounts.first.should be_a(ContextIO::Account)
     end
 
-    it 'should call the correct API request' do
+    it 'should call the API request' do
       stub = stub_request(:get, 'https://api.context.io/2.0/accounts').to_return(:body => '[]')
       ContextIO::Account.all
 
