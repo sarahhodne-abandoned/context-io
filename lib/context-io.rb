719bf81fd5725a59c9ad838477056851701eec32
Henrik Hodne <dvyjones@dvyjones.com>
2012-01-18 10:20:47 +0100

    We need to require the file library. Oops.

--- lib/context-io.rb
+++ lib/context-io.rb
@@ -1,6 +1,7 @@
 require 'context-io/config'
 require 'context-io/authentication'
 require 'context-io/account'
+require 'context-io/file'
 require 'context-io/source'
 require 'context-io/message'
 
