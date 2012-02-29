fec0c2ec8e17bface5bf57792f8e0d2112b351ee
Henrik Hodne <dvyjones@dvyjones.com>
2012-02-29 22:54:59 +0000

    Add some docs, attributes and implement ConnectToken.find

--- lib/context-io/connect_token.rb
+++ lib/context-io/connect_token.rb
@@ -13,9 +13,63 @@ module ContextIO
   #   # you probably need to refetch the token here
   #   account = token.account
   class ConnectToken < Resource
+    # @api public
+    # @return [String] The token ID
+    attr_reader :token
+
+    # @api public
+    # @return [String] The email address for the attached token
+    attr_reader :email
+
+    # @api public
+    # @return [Time] When the token was created
+    attr_reader :created_at
+
+    # @api public
+    # @return [Time] When the token was used
+    attr_reader :used_at
+
+    # @api public
+    # @return [String] The callback URL to call after the mailbox is connected
+    #   to the API key.
+    attr_reader :callback_url
+
+    # @api public
+    # @return [String] The service level for the account. Can be `'basic'` or
+    #   `'pro'`.
+    attr_reader :service_level
+
+    # @api public
+    # @return [String] The first name for the account.
+    attr_reader :first_name
+
+    # @api public
+    # @return [String] The last name for the account
+    attr_reader :last_name
+
+    # @api public
+    # @return [ContextIO::Account, nil] The account connected to this token
+    attr_reader :account
+
     # Fetch all connect tokens created with your API key
+    #
+    # @api public
+    #
+    # @return [Array<ContextIO::ConnectToken>] All connect tokens created with
+    #   your API key.
     def self.all
       get('/2.0/connect_tokens').map { |obj| from_json(obj) }
+    end
+
+    # Fetch a single connect token by it's token ID
+    #
+    # @api public
+    #
+    # @param [String] token_id The token ID. Returned by {#token}.
+    #
+    # @return [ContextIO::ConnectToken] The requested connect token.
+    def self.find(token_id)
+      from_json(get("/2.0/connect_tokens/#{token_id}"))
     end
 
     # Create an ConnectToken instance from the data returned by the API
@@ -32,7 +86,7 @@ module ContextIO
         @token = data['token']
         @email = data['email']
         @created_at = Time.at(data['created'])
-        @used = data['used'] == 1
+        @used_at = data['used'] == 0 ? nil : Time.at(data['used'])
         @callback_url = data['callback_url']
         @service_level = data['service_level']
         @first_name = data['first_name']
--- spec/connect_token_spec.rb
+++ spec/connect_token_spec.rb
@@ -20,4 +20,24 @@ describe ContextIO::ConnectToken do
       @all_request.should have_been_requested
     end
   end
+
+  describe '.find' do
+    before(:each) do
+      json_token = File.read(File.expand_path('../fixtures/connect_token.json',
+                                               __FILE__))
+      url = 'https://api.context.io/2.0/connect_tokens/1234567890abcdef'
+      @single_request = stub_request(:get, url).
+        to_return(:body => json_token)
+    end
+
+    it 'returns a ConnectToken object' do
+      token = ContextIO::ConnectToken.find('1234567890abcdef')
+      token.should be_a(ContextIO::ConnectToken)
+    end
+
+    it 'calls the API request' do
+      ContextIO::ConnectToken.find('1234567890abcdef')
+      @single_request.should have_been_requested
+    end
+  end
 end
